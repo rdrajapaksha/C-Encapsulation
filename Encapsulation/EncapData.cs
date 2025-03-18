cdb793c67e52440a8bc15d3c94a7c2f09528dc98
ravindudilhara99 <rdrajapaksha67@gmail.com>
2025-03-18 12:34:13 +0530

    Updated files

--- Encapsulation/EncapData.cs
+++ Encapsulation/EncapData.cs
@@ -9,11 +9,28 @@ namespace Encapsulation
     class EncapData
     {
         private double radius;
-        private double pi = 3.147;
+        //private double pi = 3.147;
+        private double pi = Math.PI;
+
         public double Radius
         {
             get { return radius; }
             set { radius = value; }
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public double circum
+        {
+            get { return radius * pi * 2; }
+        }
+
+        public double Area
+        {
+            get { return pi * radius * radius; }
         }
     }
 }
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -10,17 +10,24 @@ namespace lec04Recap
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static double radius;
+        public static void Main(string[] args)
         {
-            //taken a user input for the radius
-            Console.WriteLine("Enter the radius of the circle: ");
-            double radius = double.Parse(Console.ReadLine());
+            // taken a user input for the radius
 
-            //pass it to the EncapData class
+            Console.WriteLine("Enter the radius: ");
+            radius = double.Parse(Console.ReadLine());
+
+            // pass it to the EncapData class
             EncapData data1 = new EncapData();
-            //data1.Radius = radius;//data1.Radius is a private data member
+            // data1.radius = radius; // data1.radius is a private data member
             data1.Radius = radius;
 
+            Console.WriteLine("AREA: " + data1.Area);
+            Console.WriteLine("Circumference: " + data1.circum);
+
+
+            Console.ReadLine();
         }
     }
 
